8656a0409de2365a21847b7c68312ea4b8aa83af
Kyson Briggs <kysonjbriggs@gmail.com>
2021-09-10 09:47:36 -0600

    fixed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.get('/css', (req, res) => {
+app.get('/index.css', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.css'))
 })
 
